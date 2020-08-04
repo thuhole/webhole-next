50a7a7b06cd8ac36d6135c9dd4f7b47005aa14a2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-04 07:10:59 +0000

    Deploy next.thuhole.com to github.com/thuhole/webhole-next.git:gh-pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "manifest.json": "413275afdc6fe9fdc8362ac43e739e76",
-"main.dart.js": "35f736b2c69bdff22779fb1ba5e47e7c",
+"main.dart.js": "3758c05016b8ca54511b5908a09c7b2b",
 "index.html": "825c16304bb40bd9fc5b1e536bcdcd67",
 "/": "825c16304bb40bd9fc5b1e536bcdcd67",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
