a286b58e36ceb78ebabcf9ea3a0c930253dda0a9
thuhole <thuhole@protonmail.com>
2020-08-04 14:23:45 +0800

    add basic features

--- lib/network.dart
+++ lib/network.dart
@@ -26,13 +26,16 @@ class PostsFetcher extends HoleFetcher {
             await tokenParams(token),
         headers: {HttpHeaders.userAgentHeader: UA});
     if (resp.statusCode == 200) {
-      return json.decode(resp.body)["data"];
+      dynamic j = json.decode(resp.body);
+      if (j["code"] != 0) {
+        throw Exception(j["msg"]);
+      }
+      return j["data"];
     } else {
-      throw Exception('HTTP代码' + resp.statusCode.toString());
+      throw Exception('HTTP异常代码' + resp.statusCode.toString());
     }
   }
 }
-
 
 class AttentionFetcher extends HoleFetcher {
   Future<List<dynamic>> fetch(int page) async {
@@ -51,9 +54,38 @@ class AttentionFetcher extends HoleFetcher {
             await tokenParams(token),
         headers: {HttpHeaders.userAgentHeader: UA});
     if (resp.statusCode == 200) {
-      return json.decode(resp.body)["data"];
+      dynamic j = json.decode(resp.body);
+      if (j["code"] != 0) {
+        throw Exception(j["msg"]);
+      }
+      return j["data"];
     } else {
-      throw Exception('HTTP代码' + resp.statusCode.toString());
+      throw Exception('HTTP异常代码' + resp.statusCode.toString());
+    }
+  }
+}
+
+class CommentFetcher {
+  Future<List<dynamic>> fetch(int pid) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString('thuToken');
+    if (token == null || token.length != 32) {
+      throw Exception("尚未登录");
+    }
+    final resp = await http.get(
+        THUHOLE_API_BASE +
+            "/api.php?action=getcomment&pid=" +
+            pid.toString() +
+            await tokenParams(token),
+        headers: {HttpHeaders.userAgentHeader: UA});
+    if (resp.statusCode == 200) {
+      dynamic j = json.decode(resp.body);
+      if (j["code"] != 0) {
+        throw Exception(j["msg"]);
+      }
+      return j["data"];
+    } else {
+      throw Exception('HTTP异常代码' + resp.statusCode.toString());
     }
   }
 }
