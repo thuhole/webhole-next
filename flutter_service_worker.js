6e05cc07d74ba1c206e8216491aae3f792ffa596
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-04 06:26:47 +0000

    Deploy thuhole/webhole-next to github.com/thuhole/webhole-next.git:gh-pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,14 +4,14 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "manifest.json": "413275afdc6fe9fdc8362ac43e739e76",
-"main.dart.js": "0f5df9dbc54084b1093c22302fac3add",
+"main.dart.js": "dcb973fb907d00610d9845eda3983f94",
 "index.html": "825c16304bb40bd9fc5b1e536bcdcd67",
 "/": "825c16304bb40bd9fc5b1e536bcdcd67",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/AssetManifest.json": "3f2a7e99c2cf5a853aac1b834c69e2b2",
 "assets/assets/a.jpg": "a3a48127099717306a9f71f3c3aa7449",
-"assets/NOTICES": "e12a7370a9de3ab8547ec47c206e9996",
+"assets/NOTICES": "37a2619ea9b24d1c9a61988259fa8799",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
